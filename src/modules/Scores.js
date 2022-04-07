f6753aed88d38dfe73cdb1f8692910ce9def47af
fasaldana <fernando_s_98@hotmail.com>
2022-04-07 16:49:51 -0500

    Fixed linters errors

--- src/modules/Scores.js
+++ src/modules/Scores.js
@@ -1,28 +1,24 @@
 import axios from 'axios';
-const url =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/j9Z2IwQVH3jsmZjNAYhp/scores/';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 var list = document.querySelector('.scores-table');
 const table = document.getElementById('table');
+var cell, row, cellText;
 
 const listTable = async () => {
   try {
-    const resPost = await axios(url);
-    console.log(resPost);
+    const resPost = await axios(`${url}/games/j9Z2IwQVH3jsmZjNAYhp/scores/`);
     for (var i = 0; i < resPost.data.result.length; i += 1) {
-      var row = document.createElement('tr');
+      row = document.createElement('tr');
       for (var j = 0; j < 2; j += 1) {
         if (j === 0) {
-          var cell = document.createElement('td');
-          var cellText = document.createTextNode(
-            `${resPost.data.result[i].user}`
-          );
+          cell = document.createElement('td');
+          cellText = document.createTextNode(`${resPost.data.result[i].user}`);
           cell.appendChild(cellText);
           row.appendChild(cell);
         } else if (j === 1) {
-          var cell = document.createElement('td');
-          var cellText = document.createTextNode(
-            `${resPost.data.result[i].score}`
-          );
+          cell = document.createElement('td');
+          cellText = document.createTextNode(`${resPost.data.result[i].score}`);
           cell.appendChild(cellText);
           row.appendChild(cell);
         }
--- src/modules/sendData.js
+++ src/modules/sendData.js
@@ -1,12 +1,9 @@
-import axios from 'axios';
-
-const url =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/j9Z2IwQVH3jsmZjNAYhp/scores/';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 
 const sendData = async () => {
   const userName = document.getElementById('name-input').value;
   const userScore = document.getElementById('score-input').value;
-  const response = await fetch(url, {
+  const response = await fetch(`${url}/games/j9Z2IwQVH3jsmZjNAYhp/scores/`, {
     method: 'POST',
     body: JSON.stringify({
       user: userName,
